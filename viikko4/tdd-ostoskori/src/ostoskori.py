e60c6a574efe866f012fd27d101c9d753583aca0
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-25 10:15:35 +0200

    Add ability to add products to ostoskori

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -7,21 +7,24 @@ class Ostoskori:
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.ostokset)
+        res = 0
+        for ostos in self.ostokset:
+            res += ostos.lukumaara()
+        return res
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
         res = 0
-        for tuote in self.ostokset:
-            res += tuote.hinta()
+        for ostos in self.ostokset:
+            res += ostos.hinta() * ostos.lukumaara()
         return res
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        pass
+        self.ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
