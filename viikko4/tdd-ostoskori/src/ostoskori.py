11aba20c1299ed492a7c509b7c45a8fb9dc7b5de
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-25 12:01:14 +0200

    Add test for two of the same product being one ostos

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -18,12 +18,17 @@ class Ostoskori:
     def hinta(self):
         res = 0
         for ostos in self._ostokset:
-            res += ostos.hinta() * ostos.lukumaara()
+            res += ostos.hinta() 
         return res
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
+        for ostos in self._ostokset:
+            if ostos.tuotteen_nimi() == lisattava.nimi():
+                ostos.muuta_lukumaaraa(1)
+                return
+
         self._ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
