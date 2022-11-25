5aa874a5a23cb4cd4a4dc44dad711cf1d2c1f148
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-25 10:25:50 +0200

    Add test for ostoskori price matching price of two separate products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -29,3 +29,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(voi)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_hinta_vasta_tavaroiden_hintoja(self):
+        maito = Tuote("Maito", 3)
+        voi = Tuote("Voi", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(voi)
+
+        self.assertEqual(self.kori.hinta(), 8)
