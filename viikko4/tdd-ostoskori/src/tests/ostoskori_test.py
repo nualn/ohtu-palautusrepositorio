d6d23f9a7d83516f2a9348c3d360c3eee7e8318c
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-25 12:04:00 +0200

    Fix ostosolio count functionality

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -79,3 +79,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_ostos_olio_jolla_oikea_nimi_ja_lukumaara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.lukumaara(), 2)
+        self.assertAlmostEqual(ostos.tuotteen_nimi(), "Maito")
