9eda09f8ad2c394202617430c45559f44a421524
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-25 10:22:39 +0200

    Add test for product count being two for two separate products in ostoskori

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,3 +21,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        voi = Tuote("Voi", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(voi)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
