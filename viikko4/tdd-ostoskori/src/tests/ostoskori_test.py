e541239813073d97fd11966a6566a1d1f75f87e6
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-25 10:41:43 +0200

    Add test for adding two products in ostoskori leading to two ostosolios

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -64,3 +64,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.lukumaara(), 1)
         self.assertAlmostEqual(ostos.tuotteen_nimi(), "Maito")
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosoliota(self):
+        maito = Tuote("Maito", 3)
+        voi = Tuote("Voi", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(voi)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
